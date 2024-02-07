8c42a8241e8f3026b13eb4d21c7413e91374755a
Paavalen <p.lingachetti@gmail.com>
2024-02-07 13:11:56 +0400

    modify window to be in 16:9

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,25 +1,46 @@
 package main;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.layout.StackPane;
 import javafx.scene.canvas.*;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 
 public class Main extends Application {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage stage){
+    public void start(Stage stage) {
         stage.setTitle("Light Souls");
+
         StackPane pane = new StackPane();
-        Canvas canvas = new Canvas(0, 0)
-        Scene scene = new Scene(pane,800,600);
+        Canvas canvas = new Canvas();
+
+        double aspectRatio = 16.0 / 9.0;
+
+        // Listener to maintain 16:9 aspect ratio
+        stage.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldWidth, Number newWidth) {
+                double newHeight = newWidth.doubleValue() / aspectRatio;
+                stage.setHeight(newHeight);
+            }
+        });
+
+        canvas.widthProperty().bind(pane.widthProperty());
+        canvas.heightProperty().bind(pane.heightProperty());
+
+        pane.getChildren().add(canvas);
+
+        Scene scene = new Scene(pane, 800, 600);
         stage.setScene(scene);
-        stage.setFullScreen(true);
+        stage.setResizable(true);
+
         stage.show();
     }
 }
