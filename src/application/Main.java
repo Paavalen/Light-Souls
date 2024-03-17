c5d4899d4ce45226c58244180bd979e35e665617
Paavalen <p.lingachetti@gmail.com>
2024-03-17 14:53:01 +0400

    remove dead code

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,6 @@
 package application;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
